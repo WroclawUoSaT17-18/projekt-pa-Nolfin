7640b8e2f34987c5e48c9dd5a861bd68cdf7c855
Daniel Szajkowski <32747035+Nolfin@users.noreply.github.com>
2017-11-09 17:10:36 +0100

    Próba stworzenia okręgu
    Okrąg się nie pojawia po wciśnięciu przycisku

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Powietrze
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DrawIt();
+        }
+
+        private void DrawIt()
+        {
+            Graphics graphics = this.CreateGraphics();
+            System.Drawing.Rectangle rectangle = new System.Drawing.Rectangle(50, 100, 150, 150);
+            graphics.DrawEllipse(System.Drawing.Pens.Black, rectangle);
+            graphics.DrawRectangle(System.Drawing.Pens.Red, rectangle);
+        }
+
+        private Graphics CreateGraphics()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
