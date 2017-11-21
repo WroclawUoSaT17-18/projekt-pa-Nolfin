36826c2f6fd53c9b26b665e56f06da14623ef2bd
TSzymanskiPWR <tomasz.szymanski@pwr.edu.pl>
2017-11-21 09:34:13 +0100

    WPF demonstracyjny
    Proszę spojrzeć w jaki sposób wygląda prawidłowo skonstruowana architektura WPFa oraz inicjalizacja i rysowanie obiektu o zdanym kształcie.

---
+++ Przykładowa architektura aplikacji WPF _TS/wpf/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            int sz = Convert.ToInt32(Sz.Text);
+            int wy = Convert.ToInt32(Wy.Text);
+            // Create a StackPanel to contain the shape.
+            StackPanel myStackPanel = new StackPanel();
+
+            // Create a red Ellipse.
+            Ellipse myEllipse = new Ellipse();
+
+            // Create a SolidColorBrush with a red color to fill the 
+            // Ellipse with.
+            SolidColorBrush mySolidColorBrush = new SolidColorBrush();
+
+            // Describes the brush's color using RGB values. 
+            // Each value has a range of 0-255.
+            mySolidColorBrush.Color = Color.FromArgb(255, 255, 255, 0);
+            myEllipse.Fill = mySolidColorBrush;
+            myEllipse.StrokeThickness = 2;
+            myEllipse.Stroke = Brushes.Black;
+
+            // Set the width and height of the Ellipse.
+            myEllipse.Width = wy;
+            myEllipse.Height = sz;
+
+            // Add the Ellipse to the StackPanel.
+            myStackPanel.Children.Add(myEllipse);
+
+            this.Content = myStackPanel;
+        }
+    }
+}
