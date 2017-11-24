c5126cdd960f2c26b9cf253090c4251de4d12444
Daniel Szajkowski <32747035+Nolfin@users.noreply.github.com>
2017-11-24 22:32:54 +0100

    Okrąg
    zmiana formatu, okrąg się nie wyświetla

---
+++ WindowsFormsApp3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace WindowsFormsApp3
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+    public static class GraphicsExtensions
+    {
+        public static void DrawCircle(this Graphics g, Pen pen, float centerX = 500, float centerY = 500, float radius = 500)
+        {
+            g.DrawEllipse(pen, centerX - radius, centerY - radius, radius + radius, radius + radius);
+        }
+    }
+}
